9c9d571c312e3405654e38d60ca8535a0bdd4e86
jcrawford3 <95257254+jcrawford3@users.noreply.github.com>
2022-02-01 21:32:24 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,60 @@
+function formatDate() {
+  let h3 = document.querySelector("h3");
+  let now = new Date();
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let day = days[now.getDay()];
+  let hours = now.getHours();
+  let minutes = now.getMinutes();
+
+  h3.innerHTML = `Today is ${day} ${hours}:${minutes}`;
+}
+
+formatDate();
+
+function showTemperature(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+}
+
+function searchCity(city) {
+  let apiKey = "0821ab50d69ba23a98b1bd9d0cf0c1e4";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showTemperature);
+}
+
+function showSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-text-input").value;
+  searchCity(city);
+}
+
+function searchLocation(position) {
+  let apiKey = "0821ab50d69ba23a98b1bd9d0cf0c1e4";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}units=metric`;
+  axios.get(apiUrl).then(showTemperature);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+let inputCity = document.querySelector("#input-city");
+inputCity.addEventListener("submit", showSubmit);
+
+let currentLocationButton = document.querySelector("#current-location-btn");
+currentLocationButton.addEventListener("click", getCurrentLocation);
+
+searchCity("New York");
